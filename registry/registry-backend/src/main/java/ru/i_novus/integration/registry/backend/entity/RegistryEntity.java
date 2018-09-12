25f7eeba1c5fd1c7a564e0a44ccfa2d903e9c7d1
Samoilov Anton <asamoilov@i-novus.ru>
2018-09-12 16:03:44 +0300

    NF IBUS-22 регистрация и изменение записи

--- registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/entity/RegistryEntity.java
+++ registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/entity/RegistryEntity.java
@@ -13,8 +13,14 @@ public class RegistryEntity {
     @Column(name = "code")
     private String code;
 
+    @Column(name = "name")
+    private String name;
+
     @Column(name = "url")
     private String url;
+
+    @Column(name = "disable")
+    private Boolean disable;
 
 
     public String getCode() {
@@ -25,11 +31,27 @@ public class RegistryEntity {
         this.code = code;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getUrl() {
         return url;
     }
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public Boolean isDisable() {
+        return disable;
+    }
+
+    public void setDisable(Boolean disable) {
+        this.disable = disable;
     }
 }
--- registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/repository/RegistryRepository.java
+++ registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/repository/RegistryRepository.java
@@ -1,12 +1,17 @@
 package ru.i_novus.integration.registry.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.i_novus.integration.registry.backend.entity.RegistryEntity;
+
+import java.util.Optional;
 
 
 @Repository
 public interface RegistryRepository extends JpaRepository<RegistryEntity, String> {
-    /*@Query("select r.code from RegistryEntity r where r.host = ?1")
-    Optional<String> findCodeByHost(@Param("host") String host);*/
+    @Query("select r from RegistryEntity r where (r.disable = false or r.disable is null) and r.code = ?1")
+    Optional<RegistryEntity> findById(@Param("code") String code);
 }
+
