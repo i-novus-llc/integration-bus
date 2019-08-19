a9cff1905bfef7f633ee417e62a8bfa0dd65d62b
asamoilov <13865>
2019-08-19 17:42:29 +0300

    NF IBUS-95 example
    (cherry picked from commit 5c33e483b5a39cd9185f02a695843b85fd207511)

---
+++ integration-example/src/test/java/ru/i_novus/is/example/IntegrationExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.i_novus.is.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegrationExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
