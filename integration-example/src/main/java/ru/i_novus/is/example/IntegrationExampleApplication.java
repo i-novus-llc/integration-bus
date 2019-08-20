13037b4dc3ad7b93a5e900ab003a69f1b5d6d8da
asamoilov <13865>
2019-08-20 14:12:23 +0300

    СF IBUS-95 исправлен example
    (cherry picked from commit 9a74f10c90d45c0f7ca4656dcc94e68a35ec3b55)

--- integration-example/src/main/java/ru/i_novus/is/example/IntegrationExampleApplication.java
+++ integration-example/src/main/java/ru/i_novus/is/example/IntegrationExampleApplication.java
@@ -1,13 +1,27 @@
 package ru.i_novus.is.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class IntegrationExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntegrationExampleApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        restTemplate.getMessageConverters().add(converter);
+
+        return restTemplate;
     }
 
 }
