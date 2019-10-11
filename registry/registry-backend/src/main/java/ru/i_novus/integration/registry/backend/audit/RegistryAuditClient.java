18c94a0e932c9b1e1ed72157cb889eeb1bc3e52b
estartcev <estartcev@i-novus.ru>
2019-10-11 16:09:40 +0300

    [EPMP-93] Добавлен аудит сервисов

--- registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/audit/RegistryAuditClient.java
+++ registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/audit/RegistryAuditClient.java
@@ -3,6 +3,7 @@ package ru.i_novus.integration.registry.backend.audit;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Component;
 import ru.i_novus.ms.audit.client.AuditClient;
 import ru.i_novus.ms.audit.client.model.AuditClientRequest;
@@ -13,10 +14,12 @@ public class RegistryAuditClient {
     private AuditClient auditClient;
     @Autowired
     private ObjectMapper mapper;
+    @Autowired
+    private MessageSourceAccessor messageSourceAccessor;
 
     public void audit(String action, Object object, String objectId, String objectName) {
         AuditClientRequest request = new AuditClientRequest();
-        request.setEventType(action);
+        request.setEventType(messageSourceAccessor.getMessage(action));
         request.setObjectType(object.getClass().getSimpleName());
         request.setObjectId(objectId);
         try {
