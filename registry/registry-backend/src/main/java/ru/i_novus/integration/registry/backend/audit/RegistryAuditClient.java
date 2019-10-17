29e0997116523d94b1922b99932a11fec46ea5ed
estartcev <estartcev@i-novus.ru>
2019-10-17 13:41:38 +0300

    [EPMP-639] Убрано получение пользователя

--- registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/audit/RegistryAuditClient.java
+++ registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/audit/RegistryAuditClient.java
@@ -2,14 +2,11 @@ package ru.i_novus.integration.registry.backend.audit;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.security.core.context.SecurityContextHolder;
 import ru.i_novus.ms.audit.client.AuditClient;
 import ru.i_novus.ms.audit.client.model.AuditClientRequest;
-import net.n2oapp.security.auth.common.User;
 
 @Component
 public class RegistryAuditClient {
@@ -33,25 +30,7 @@ public class RegistryAuditClient {
         request.setObjectName(messageSourceAccessor.getMessage(objectName));
         request.setAuditType((short) 1);
 
-        User user = getUser();
-        if (user != null) {
-            request.setUserId(user.getEmail());
-            request.setUsername(user.getUsername());
-        }
-
         auditClient.add(request);
-    }
-
-    private User getUser() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null && auth.getPrincipal() != null) {
-            if (auth.getPrincipal() instanceof User) {
-                return (User) auth.getPrincipal();
-            } else {
-                return new User("" + auth.getPrincipal());
-            }
-        }
-        return null;
     }
 
 }
