5e5ad381999d4b9022d7e471356781167321c036
estartcev <estartcev@i-novus.ru>
2019-10-17 13:16:12 +0300

    [EPMP-630] Обновление версии интеграционной шины

--- integration/src/main/java/ru/i_novus/integration/model/AbstractRequestModel.java
+++ integration/src/main/java/ru/i_novus/integration/model/AbstractRequestModel.java
@@ -1,42 +1,15 @@
 package ru.i_novus.integration.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
 
+@Getter
+@Setter
 public abstract class AbstractRequestModel {
     private String uid;
     private List<String> recipient;
     private String method;
     private String envCode;
 
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public List<String> getRecipient() {
-        return recipient;
-    }
-
-    public void setRecipient(List<String> recipient) {
-        this.recipient = recipient;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    public String getEnvCode() {
-        return envCode;
-    }
-
-    public void setEnvCode(String envCode) {
-        this.envCode = envCode;
-    }
 }
