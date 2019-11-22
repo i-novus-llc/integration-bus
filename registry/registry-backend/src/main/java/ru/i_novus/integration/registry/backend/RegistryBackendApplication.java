3d3107d75e7dd1d13098c5c4d7162962c24f8786
estartcev <estartcev@i-novus.ru>
2019-11-22 10:58:27 +0300

    [EPMP-916] Убран кастомный loader

--- registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/RegistryBackendApplication.java
+++ registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/RegistryBackendApplication.java
@@ -1,17 +1,11 @@
 package ru.i_novus.integration.registry.backend;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.n2oapp.platform.loader.server.ServerLoader;
-import net.n2oapp.platform.loader.server.ServerLoaderRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import ru.i_novus.integration.registry.backend.loader.JsonLocalLoaderRunner;
 
-import java.util.List;
 
 @SpringBootApplication
 @ComponentScan({"ru.i_novus.integration.registry.backend"})
@@ -23,8 +17,4 @@ public class RegistryBackendApplication {
         SpringApplication.run(RegistryBackendApplication.class, args);
     }
 
-    @Bean
-    public ServerLoaderRunner localLoader(List<ServerLoader> loaders) {
-        return new JsonLocalLoaderRunner(loaders, new ObjectMapper());
-    }
 }
