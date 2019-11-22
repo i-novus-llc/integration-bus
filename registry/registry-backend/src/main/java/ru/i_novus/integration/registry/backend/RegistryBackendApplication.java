6435c8de38523f31e6896601b8945b8b17d4a0a4
estartcev <estartcev@i-novus.ru>
2019-11-22 10:00:18 +0300

    [EPMP-916] Добавлен loader with utf-8 reader

--- registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/RegistryBackendApplication.java
+++ registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/RegistryBackendApplication.java
@@ -1,10 +1,17 @@
 package ru.i_novus.integration.registry.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.n2oapp.platform.loader.server.ServerLoader;
+import net.n2oapp.platform.loader.server.ServerLoaderRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ru.i_novus.integration.registry.backend.loader.JsonLocalLoaderRunner;
+
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan({"ru.i_novus.integration.registry.backend"})
@@ -14,5 +21,10 @@ public class RegistryBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegistryBackendApplication.class, args);
+    }
+
+    @Bean
+    public ServerLoaderRunner localLoader(List<ServerLoader> loaders) {
+        return new JsonLocalLoaderRunner(loaders, new ObjectMapper());
     }
 }
