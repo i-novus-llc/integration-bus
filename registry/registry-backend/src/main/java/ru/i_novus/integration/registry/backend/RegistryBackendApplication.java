f2718c9f755164899b854b2ffa33edfdff489999
asamoilov <13865>
2020-11-30 15:37:41 +0300

    СF IBUS-190 исправление замечаний

--- registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/RegistryBackendApplication.java
+++ registry/registry-backend/src/main/java/ru/i_novus/integration/registry/backend/RegistryBackendApplication.java
@@ -3,12 +3,10 @@ package ru.i_novus.integration.registry.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class RegistryBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegistryBackendApplication.class, args);
     }
-
 }
