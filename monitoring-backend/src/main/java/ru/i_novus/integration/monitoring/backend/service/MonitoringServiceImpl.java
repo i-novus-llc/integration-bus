d22d7c6cadfd5ebb5ade5628d313fd891328769c
asamoilov <13865>
2021-01-13 12:45:51 +0300

    СF IBUS-195 выпуск версии

---
+++ monitoring-backend/src/main/java/ru/i_novus/integration/monitoring/backend/service/MonitoringServiceImpl.java
@@ -0,0 +1,78 @@
+package ru.i_novus.integration.monitoring.backend.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import ru.i_novus.integration.common.api.model.MonitoringModel;
+import ru.i_novus.integration.monitoring.api.criteria.SentMessageCriteria;
+import ru.i_novus.integration.monitoring.api.criteria.SentMessageStageCriteria;
+import ru.i_novus.integration.monitoring.backend.entity.SentMessageEntity;
+import ru.i_novus.integration.monitoring.backend.entity.SentMessageStageEntity;
+import ru.i_novus.integration.monitoring.api.model.ErrorModel;
+import ru.i_novus.integration.monitoring.api.model.SentMessageModel;
+import ru.i_novus.integration.monitoring.api.model.SentMessageStageModel;
+import ru.i_novus.integration.monitoring.backend.repository.SentMessageRepository;
+import ru.i_novus.integration.monitoring.backend.repository.SentMessageSpecifications;
+import ru.i_novus.integration.monitoring.backend.repository.SentMessageStageRepository;
+import ru.i_novus.integration.monitoring.backend.repository.SentMessageStageSpecifications;
+import ru.i_novus.integration.monitoring.api.service.MonitoringService;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class MonitoringServiceImpl implements MonitoringService {
+
+    private SentMessageRepository sentMessageRepository;
+    private SentMessageStageRepository sentMessageStageRepository;
+
+    @Override
+    public Page<SentMessageModel> findAll(SentMessageCriteria criteria) {
+        return findSentMessage(criteria).map(SentMessageEntity::getSentMessageModel);
+    }
+
+    @Override
+    public Page<SentMessageStageModel> monitoringFormByUid(SentMessageStageCriteria criteria) {
+        return findSentMessageStage(criteria).map(SentMessageStageEntity::fillSentMessageStageModel);
+    }
+
+    @Override
+    public ErrorModel getErrorStackTrace(Integer id) {
+        ErrorModel errorModel = new ErrorModel();
+        SentMessageStageEntity sentMessageStageEntity = sentMessageStageRepository.findById(id).orElseThrow();
+        errorModel.setId(sentMessageStageEntity.getId());
+        errorModel.setError(sentMessageStageEntity.getError());
+        return errorModel;
+    }
+
+    @Override
+    public SentMessageStageModel save(MonitoringModel model) {
+        Optional<SentMessageEntity> messageEntity = sentMessageRepository.findByUidAndSenderAndReceiver(model.getUid(),
+                model.getSender(), model.getReceiver());
+        Integer sentMessageId;
+        if (messageEntity.isPresent()) {
+            messageEntity.get().setCurrentStatus(model.getStatus());
+            sentMessageId = messageEntity.get().getId();
+        } else {
+            sentMessageId = sentMessageRepository.save(new SentMessageEntity(model.getUid(), model.getDateTime(),
+                    model.getSender(), model.getReceiver(), model.getOperation(), model.getStatus(), model.getComment())).getId();
+        }
+        return sentMessageStageRepository.save(new SentMessageStageEntity(sentMessageId, model.getDateTime(), model.getStatus(),
+                model.getError(), model.getComment())).fillSentMessageStageModel();
+
+    }
+
+    private Page<SentMessageEntity> findSentMessage(SentMessageCriteria criteria) {
+        PageRequest pageable = PageRequest.of(criteria.getPageNumber(), criteria.getPageSize(),
+                criteria.getSort());
+        return sentMessageRepository.findAll(SentMessageSpecifications.equalCriteriaParams(criteria), pageable);
+    }
+
+
+    private Page<SentMessageStageEntity> findSentMessageStage(SentMessageStageCriteria criteria) {
+        PageRequest pageable = PageRequest.of(criteria.getPageNumber(), criteria.getPageSize(),
+                criteria.getSort());
+        return sentMessageStageRepository.findAll(SentMessageStageSpecifications.equalCriteriaParams(criteria), pageable);
+    }
+}
---
+++ registry-backend/src/main/java/ru/i_novus/integration/registry/backend/rest/ParticipantMethodRestServiceImpl.java
@@ -0,0 +1,74 @@
+package ru.i_novus.integration.registry.backend.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Controller;
+import ru.i_novus.integration.registry.api.rest.ParticipantMethodRestService;
+import ru.i_novus.integration.registry.api.criteria.ParticipantMethodCriteria;
+import ru.i_novus.integration.registry.backend.entity.ParticipantMethodEntity;
+import ru.i_novus.integration.registry.api.model.IntegrationType;
+import ru.i_novus.integration.registry.api.model.ParticipantMethod;
+import ru.i_novus.integration.registry.backend.repository.IntegrationTypeRepository;
+import ru.i_novus.integration.registry.backend.repository.ParticipantMethodRepository;
+import ru.i_novus.integration.registry.backend.specifications.ParticipantMethodSpecifications;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class ParticipantMethodRestServiceImpl implements ParticipantMethodRestService, Mappers {
+
+    private final ParticipantMethodRepository repository;
+
+    private final IntegrationTypeRepository integrationTypeRepository;
+
+    @Autowired
+    public ParticipantMethodRestServiceImpl(ParticipantMethodRepository repository, IntegrationTypeRepository integrationTypeRepository) {
+        this.repository = repository;
+        this.integrationTypeRepository = integrationTypeRepository;
+    }
+
+    @Override
+    public Page<ParticipantMethod> findAll(ParticipantMethodCriteria criteria) {
+        Specification<ParticipantMethodEntity> specification = new ParticipantMethodSpecifications(criteria);
+
+        Page<ParticipantMethodEntity> participants = repository.findAll(specification, criteria);
+        return participants.map(this::map);
+    }
+
+    @Override
+    public ParticipantMethod getById(Integer id) {
+        return map(repository.findById(id).orElse(null));
+    }
+
+    @Override
+    public ParticipantMethod create(ParticipantMethod participant) {
+        ParticipantMethodEntity result = repository.save(map(participant));
+        return map(result);
+    }
+
+    @Override
+    public ParticipantMethod update(ParticipantMethod participantMethod) {
+        ParticipantMethodEntity entity = repository.findById(participantMethod.getId()).orElse(null);
+        if (entity == null) {
+            throw new IllegalArgumentException("Can't find participantMethod by id " + participantMethod.getId());
+        }
+        entity.setDisable(participantMethod.getDisable());
+        entity.setIntegrationType(map(participantMethod.getIntegrationType()));
+        entity.setMethodCode(participantMethod.getMethodCode());
+        entity.setUrl(participantMethod.getUrl());
+        return map(repository.save(entity));
+    }
+
+    @Override
+    public void delete(Integer code) {
+        repository.findById(code).ifPresent(this::map);
+        repository.deleteById(code);
+    }
+
+    @Override
+    public List<IntegrationType> getAllIntegrationTypes() {
+        return integrationTypeRepository.findAll().stream().map(this::map).collect(Collectors.toList());
+    }
+}
